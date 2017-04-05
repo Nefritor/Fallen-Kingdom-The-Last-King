ab21406988b6962caf99b4de6b0cafe4159b606c
Марат Хамитов <nefritor@gmail.com>
2017-04-05 23:14:15 +0500

    Билд 5 - Добавил индикаторы положения солнца и луны

--- Assets/Scripts/MoonCycle.cs
+++ Assets/Scripts/MoonCycle.cs
@@ -6,11 +6,16 @@ public class MoonCycle : MonoBehaviour {
 
 	public float MoonCycleSpeed = 3;
 	public Transform sunLight;
+    public RectTransform moonRect;
 
-	void Update () {
+    void Update () {
 		transform.RotateAround (Vector3.zero, new Vector3 (2,5,4), MoonCycleSpeed * Time.deltaTime);
 		transform.LookAt (Vector3.zero);
-		if (sunLight.position.y >= 0) {
+
+        float xPos = -transform.position.x + transform.position.z;
+        moonRect.localPosition = new Vector3(xPos / 8, transform.position.y / 15 + 40, 0);
+
+        if (sunLight.position.y >= 0) {
 			GetComponent<Light> ().shadowStrength -= 0.5f * Time.deltaTime;
 		} else GetComponent<Light> ().shadowStrength += 0.5f * Time.deltaTime;
 		GetComponent<Light> ().shadowStrength = Mathf.Clamp (GetComponent<Light> ().shadowStrength, 0, 0.5f);
--- Assets/Scripts/SunCycle.cs
+++ Assets/Scripts/SunCycle.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class SunCycle : MonoBehaviour {
 
 	public float SunCycleSpeed = 10;
+    public RectTransform sunRect;
 
-	void Update () {
+    void Update () {
 		transform.RotateAround (Vector3.zero, new Vector3 (6,6,6), SunCycleSpeed * Time.deltaTime);
 		transform.LookAt (Vector3.zero);
-	}
+        float xPos = -transform.position.x + transform.position.z;
+        sunRect.localPosition = new Vector3(xPos / 8, transform.position.y / 15 + 40, 0);
+    }
 }
